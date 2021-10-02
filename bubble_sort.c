485c165ad32a2745f569b86fa01216dc8aa4ca72
Anushka Paul <87390353+pilipi-puu-puu@users.noreply.github.com>
2021-10-02 23:32:41 +0530

    Updated bubblesort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
-
-int bubble_sort(int a[], int l){
-    for(int i=l-2; i>=0; i--){
-        for(int j=0; j<=i; j++){
-            if(a[j]>a[j+1]){
-                int temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
-            }
-        }
-    }
-}
-
-void main(){
-    int a[10] = {1, 8, 2, 9, 7, 5};
-    // call bubble sort
-    bubble_sort(a, 6);
-    printf("Array: ");
-    for(int i=0; i<6; i++){
-        printf("%d ", a[i]);
-    }
-}
+#include<stdio.h>
+void main()
+{
+  int i,j,n,arr[100],temp;
+  printf("How many numbers do you want? ");
+  scanf("%d",&n);
+  
+  for(i=0;i<n;i++)
+  {
+  	printf("\nEnter the arrays:");
+  	scanf("%d",&arr[i]);
+  }
+  
+  for(i=0;i<n-1;i++)
+  for(j=0;j<n-i-1;j++)
+  
+  if(arr[j]>arr[j+1])
+  {
+  	temp=arr[j];
+  	arr[j]=arr[j+1];
+  	arr[j+1]=temp;
+  }
